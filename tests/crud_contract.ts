250385227d74bb2169ead023eb39009c118f75a4
libaice <libaice147@gmail.com>
2024-12-12 23:18:25 +0800

    update crud contract uint test

--- tests/crud_contract.ts
+++ tests/crud_contract.ts
@@ -18,18 +18,59 @@ describe("crud_contract", () => {
       payer: payer.publicKey,
     }).signers([crudKeypair]).rpc();
 
+    // 1. current count
     const currentCount = await program.account.crud.fetch(crudKeypair.publicKey)
 
     console.log("current count after initialization: ", currentCount);
 
+    // 2. count += 1
     await program.methods.increment().accounts({
       crud: crudKeypair.publicKey,
     }).rpc();
 
-    const newCount = await program.account.crud.fetch(crudKeypair.publicKey)
+    let newCount = await program.account.crud.fetch(crudKeypair.publicKey)
+
+    console.log("new count after increment: ", newCount);
+
+    // 3. count += 1
+    await program.methods.increment().accounts({
+      crud: crudKeypair.publicKey,
+    }).rpc();
+
+    newCount = await program.account.crud.fetch(crudKeypair.publicKey)
 
     console.log("new count after increment: ", newCount);
 
 
+    // 4. count -= 1
+    await program.methods.decrement().accounts({
+      crud: crudKeypair.publicKey,
+    }).rpc();
+
+    newCount = await program.account.crud.fetch(crudKeypair.publicKey)
+
+    console.log("new count after decrement: ", newCount);
+
+    // 5. set count value 
+    await program.methods.set(44).accounts({
+      crud: crudKeypair.publicKey
+    }).rpc();
+
+    newCount = await program.account.crud.fetch(crudKeypair.publicKey)
+
+    console.log("new count after set: ", newCount);
+
+    // 6. close account
+    await program.methods.close().accounts({
+      payer: payer.publicKey,
+      crud: crudKeypair.publicKey
+    }).rpc();
+
+    console.log("account closed");
+
+    const userAccount = await program.account.crud.fetchNullable(crudKeypair.publicKey)
+
+    console.log("user account after close: ", userAccount);
   });
+
 });
